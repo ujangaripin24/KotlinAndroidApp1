3304094e5b0824752c30ae916a8ef6ddab540086
ujangaripin24 <ujangarifin06@gmail.com>
2025-08-25 11:51:14 +0700

    [page] add SplashScreen.kt

---
+++ app/src/main/java/com/example/kotlinandroidapp1/ui/splash_screen/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinandroidapp1.ui.splash_screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashScreen() {
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Tunggu...", style = MaterialTheme.typography.headlineMedium)
+    }
+}
