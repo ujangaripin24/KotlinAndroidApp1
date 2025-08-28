a3c4076f0e8e911e9e2fef0e359e38da581997b1
ujangaripin24 <ujangarifin06@gmail.com>
2025-08-28 13:05:14 +0700

    [LoginViewModel.kt] add response loading, success, failure

--- app/src/main/java/com/example/kotlinandroidapp1/ui/login_screen/LoginViewModel.kt
+++ app/src/main/java/com/example/kotlinandroidapp1/ui/login_screen/LoginViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.kotlinandroidapp1.ui.login_screen
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.kotlinandroidapp1.data.remote.model.LoginResponse
+import com.example.kotlinandroidapp1.data.repository.AuthRepository
 import com.mapbox.base.common.logger.model.Message
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 sealed class LoginUiState {
     object Idle: LoginUiState()
@@ -10,5 +16,18 @@ sealed class LoginUiState {
     data class Error(val message: String) : LoginUiState()
 }
 
-class LoginViewModel {
+class LoginViewModel(private val repository: AuthRepository) : ViewModel() {
+    private val _uiState = MutableStateFlow<LoginUiState>(LoginUiState.Idle)
+    val uiState: StateFlow<LoginUiState> = _uiState
+
+    fun login(email: String, password: String) {
+        _uiState.value = LoginUiState.Loading
+        viewModelScope.launch {
+            val result = repository.login(email, password)
+            _uiState.value = result.fold(
+                onSuccess = { LoginUiState.Success(it)},
+                onFailure = { LoginUiState.Error(it.message?: "Terjadi kesalahan")}
+            )
+        }
+    }
 }
